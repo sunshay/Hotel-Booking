4d321fd783aeab93bb6441edf0677308eb6d4445
Achille kabasele <pepexykabasele@gmail.com>
2022-10-01 21:45:14 +0200

    update settings

--- parameter_hotel/settings.py
+++ parameter_hotel/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Add app hotel
+    # Add app hotel Booking
     'app_hotel.apps.AppHotelConfig',
 ]
 
