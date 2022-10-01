c88b6ce74c03eac34fd172b6184837ab0f16c2aa
Achille kabasele <pepexykabasele@gmail.com>
2022-10-01 05:53:20 +0200

    commit first

--- app_hotel/models.py
+++ app_hotel/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Room:
+    name: str
+    price: int
+    
--- app_hotel/views.py
+++ app_hotel/views.py
@@ -1,6 +1,36 @@
+from email import message
+from multiprocessing import context
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
 def home(request, template_name="hotel/index.html"):
-    return render(request, template_name)
+    context= {}
+    messages = "Hello Hotel Booking"
+    # object instance
+    r1 = Room()
+    r2 = Room()
+    r3 = Room()
+    
+    # first object
+    r1.name= "Premium King Room 1"
+    r1.price= 100
+    
+    # second object
+    r2.name= "Premium King Room 2"
+    r2.price= 130
+    
+    # third object
+    r3.name= "Premium King Room 3"
+    r3.price= 140
+    
+    # list for all object
+    list_room = [r1, r2, r3]
+    
+
+    
+    
+    context['messages'] = messages
+    context['room_lists'] = list_room
+    return render(request, template_name, context)
