540d2c7e3ac8ee5a209a4df52884d883e7c53861
Achille kabasele <pepexykabasele@gmail.com>
2022-10-01 21:27:58 +0200

    update

--- parameter_hotel/settings.py
+++ parameter_hotel/settings.py
@@ -122,7 +122,7 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = [BASE_DIR / "static"]  
 STATIC_ROOT = STATIC_ROOT = BASE_DIR / "staticfiles" 
 MEDIA_ROOT = MEDIA_ROOT = BASE_DIR/ 'media'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
