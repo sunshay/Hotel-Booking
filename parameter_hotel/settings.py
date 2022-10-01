188eba7de70e158e027297112ab6f16253b83357
Achille kabasele <pepexykabasele@gmail.com>
2022-10-01 06:14:40 +0200

    commit first configure for static and media

--- parameter_hotel/settings.py
+++ parameter_hotel/settings.py
@@ -118,8 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
+MEDIA_URL = '/media/'
 STATICFILES_DIRS = [BASE_DIR / "static"]  
 STATIC_ROOT = STATIC_ROOT = BASE_DIR / "staticfiles" 
+MEDIA_ROOT = MEDIA_ROOT = BASE_DIR/ 'media'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
