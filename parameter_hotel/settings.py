cfc1a842fead7e583c5c3214d0105e26c80d40df
Achille kabasele <pepexykabasele@gmail.com>
2022-10-01 05:30:43 +0200

    commit first

--- parameter_hotel/settings.py
+++ parameter_hotel/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app_hotel'
+    'app_hotel.apps.AppHotelConfig',
 ]
 
 MIDDLEWARE = [
