210b89cff6821aa83ce9620b5de9ff9c16248478
Achille kabasele <pepexykabasele@gmail.com>
2022-10-03 08:15:00 +0200

    Update admin template text

--- app_hotel/admin.py
+++ app_hotel/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Person, Room,Comment,Booking, Category, Blog
 
-
-
+# change Django administration text Login, The listview page and The HTML title tag 
+admin.site.site_header = "Hotel Booking"
+admin.site.site_title = "Hotel Booking Admin Penal"
+admin.site.index_title = "Welcome to Hotel Booking"
     
 # Model admin person.
 @admin.register(Person)
